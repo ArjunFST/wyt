2afbd3f1199efb2c99f5f7cd69e62f5ca1797303
ArjunFST <arjun.ks@fullstackio.com>
2024-11-17 21:00:35 +0530

    first commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,40 @@
+import "./globals.css";
+import { Sora, Inter, Playfair_Display } from "next/font/google";
+import InnovationsCard from "./components/innovation/InnovationsCard";
+import Events from './components/Events/Events'
+import Footer from "./components/footer/Footer";
+import CoursesSection from "./components/courses/CoursesSection";
+import Form from "./components/form/Form";
+import Testimonial from "./components/testimonial/Testimonial";
+import Gallery from "./components/gallery/Gallery";
+
+const sora = Sora({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-sora",
+});
+
+const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-inter",
+});
+
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-playfair",
+});
+
+export default function Home() {
+  return (
+    <>
+      <InnovationsCard />
+      <CoursesSection />
+      <Events/>
+      <Testimonial />
+      {/* <Form /> */}
+      <Gallery />
+    </>
+  );
+}
